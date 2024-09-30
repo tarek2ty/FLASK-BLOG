d2ce68de38395e3c46e38b7660fede6fa594984e
Tarek <doda7765@gmail.com>
2024-09-30 16:31:12 +0300

    added j2 posts loop and if title

--- flaskblog.py
+++ flaskblog.py
@@ -2,14 +2,29 @@ from flask import Flask, render_template
 
 app=Flask(__name__)
 
+posts = [
+    {
+        'author':'Tarek Mohamed',
+        'title': 'Post1',
+        'content': 'First Post content',
+        'date_posted': '30 Sep 2024'
+    },
+    {
+        'author':'Anagram Mohamed',
+        'title': 'Post2',
+        'content': 'Second Post content',
+        'date_posted': '29 Sep 2024'
+    }
+]
+
 @app.route("/")
 @app.route("/home")
 def hello():
-    return render_template("home.html")
+    return render_template("home.html", posts=posts)
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html",title='about')
 
 if __name__ == '__main__':
     app.run(debug=True)
