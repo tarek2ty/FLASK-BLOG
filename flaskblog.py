746612c847a13fdd31843fb864133f4668c87b11
Tarek <doda7765@gmail.com>
2024-10-02 11:38:35 +0300

    added a form that has a bug

--- flaskblog.py
+++ flaskblog.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app=Flask(__name__)
-
+app.config['SECRET_KEY'] = 'b48b31ba403d153e7a91cf92f7a7caeb'
+#for TLS, we need a random set of chars
+#we can get from secrets.token_hex(16)
 posts = [
     {
         'author':'Tarek Mohamed',
@@ -26,6 +29,19 @@ def hello():
 @app.route("/about")
 def about():
     return render_template("about.html",title='about')
+
+@app.route("/register", methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account Created {form.username.data}!','success')
+        return redirect(url_for('home'))
+    return render_template("register.html",title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html",title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
