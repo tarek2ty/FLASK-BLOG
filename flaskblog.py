43a3d0c2d730ddc6ac6f59227991de22b707df14
Tarek <doda7765@gmail.com>
2024-10-11 13:39:53 +0300

    Database test-add the models in the main code

--- flaskblog.py
+++ flaskblog.py
@@ -1,10 +1,44 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 app=Flask(__name__)
 app.config['SECRET_KEY'] = 'b48b31ba403d153e7a91cf92f7a7caeb'
 #for TLS, we need a random set of chars
 #we can get from secrets.token_hex(16)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+#we need to provide a uri for our database
+#sql is just a file in our system; simply site.db is in our path
+#the three forward slashes: two for the uri like http://
+#and one for the relative path
+db = SQLAlchemy(app) #this is the db instance
+                     #each db structure is a class
+
+app.app_context().push()
+
+class User(db.Model): #the table of the users
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image = db.Column(db.String(20), default='default.jpg', nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+    #these are the columns for the table
+
+    def __repr__(self):
+        return f'User({self.username}, {self.email}, {self.image})'
+
+class Post(db.Model): #the table for posts
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self) -> str:
+        return f'Post({self.title}, {self.date_posted})'
+
 posts = [
     {
         'author':'Tarek Mohamed',
